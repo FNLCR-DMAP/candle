088503da5147d59e6c951b8694f11d55e882af38
Andrew Weisman <andrew.weisman@nih.gov>
2020-03-03 18:32:56 -0500

    Commiting some of George's changes to Bash template

--- templates/examples/bash/model_script.sh
+++ templates/examples/bash/model_script.sh
@@ -7,13 +7,14 @@ activation=$(candle_get_param activation)
 optimizer=$(candle_get_param optimizer)
 
 #Load any modules on Biowulf
-ml python/3.6
+module load python/3.6
 
 #Execute your program
 script_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+
 echo $script_dir
-python /data/zakigf/candle-native/bash-2/mnist_mlp.py --batch_size="$batch_size" --epochs="$epochs" --activation="$activation" --optimizer="$optimizer"
+python $script_dir/mnist_mlp.py --batch_size="$batch_size" --epochs="$epochs" --activation="$activation" --optimizer="$optimizer"
 
 #Return a value to CANDLE 
-#export CANDLE_VAL_TO_RETURN=$(cat results.txt)
-candle_val_to_return=$(cat results.txt) # no need to export as tail.sh will be appended to this file
+export CANDLE_VAL_TO_RETURN=$(cat results.txt)
+#candle_val_to_return=$(cat results.txt) # no need to export as tail.sh will be appended to this file
