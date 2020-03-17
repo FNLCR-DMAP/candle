9d313b9c9540374c07c106bf263166c0732612f9
Andrew Weisman <andrew.weisman@nih.gov>
2020-03-16 20:44:32 -0400

    About to make some changes to preprocess.py

--- templates/scripts/preprocess.py
+++ templates/scripts/preprocess.py
@@ -5,6 +5,9 @@ def check_input():
 
     import os
 
+    print('')
+    print('Keyword settings:')
+
     ################ Current keywords ######################################################################################
     # model_script
     model_script = os.getenv('MODEL_SCRIPT')
@@ -16,7 +19,7 @@ def check_input():
             print('ERROR: The file "{}" from the "model_script" keyword cannot be opened'.format(model_script))
     else:
         print('ERROR: The "model_script" keyword is not set in the &control section of the input file')
-        exit()
+        exit(1)
     print('model_script: {}'.format(model_script))
 
     # workflow
@@ -25,18 +28,18 @@ def check_input():
         workflow = workflow.lower()
         valid_workflows = ('grid', 'bayesian')
         if workflow not in valid_workflows:
-            print('ERROR: The "workflow" keyword in the &control section must be one of', valid_workflows)
-            exit()
+            print('ERROR: The "workflow" keyword ({}) in the &control section must be one of'.format(workflow), valid_workflows)
+            exit(1)
     else:
         print('ERROR: The "workflow" keyword is not set in the &control section of the input file')
-        exit()
+        exit(1)
     print('workflow: {}'.format(workflow))
 
     # walltime
     walltime = os.getenv('WALLTIME')
     if walltime is None: # don't do error-checking for walltime
         print('ERROR: The "walltime" keyword is not set in the &control section of the input file')
-        exit()
+        exit(1)
     print('walltime: {}'.format(walltime))
 
     # worker_type
@@ -46,9 +49,9 @@ def check_input():
         worker_type = worker_type.lower()
         if worker_type not in valid_worker_types:
             #print('ERROR: The "worker_type" keyword in the &control section must be one of', valid_worker_types)
-            print('WARNING: The "worker_type" keyword in the &control section must be one of', valid_worker_types)
+            print('WARNING: The "worker_type" keyword ({}) in the &control section must be one of'.format(worker_type), valid_worker_types)
             worker_type_is_valid = False
-            #exit()
+            #exit(1)
         else:
             worker_type_is_valid = True
         worker_type_is_set = True
@@ -56,9 +59,9 @@ def check_input():
         #print('ERROR: The "worker_type" keyword is not set in the &control section of the input file')
         print('WARNING: The "worker_type" keyword is not set in the &control section of the input file')
         worker_type_is_set = False
-        #exit()
-    if worker_type_is_set and worker_type_is_valid:
-        print('worker_type: {}'.format(worker_type))
+        #exit(1)
+    #if worker_type_is_set and worker_type_is_valid:
+    print('worker_type: {}'.format(worker_type))
 
     # nworkers
     nworkers = os.getenv('NWORKERS')
@@ -66,9 +69,9 @@ def check_input():
         nworkers = int(nworkers)
         if nworkers < 1:
             #print('ERROR: The "nworkers" keyword in the &control section must be a positive integer')
-            print('WARNING: The "nworkers" keyword in the &control section must be a positive integer')
+            print('WARNING: The "nworkers" keyword ({}) in the &control section must be a positive integer'.format(nworkers))
             nworkers_is_valid = False
-            #exit()
+            #exit(1)
         else:
             nworkers_is_valid = True
         nworkers_is_set = True
@@ -76,9 +79,9 @@ def check_input():
         #print('ERROR: The "nworkers" keyword is not set in the &control section of the input file')
         print('WARNING: The "nworkers" keyword is not set in the &control section of the input file')
         nworkers_is_set = False
-        #exit()
-    if nworkers_is_set and nworkers_is_valid:
-        print('nworkers: {}'.format(nworkers))
+        #exit(1)
+    #if nworkers_is_set and nworkers_is_valid:
+    print('nworkers: {}'.format(nworkers))
     ########################################################################################################################
 
 
@@ -89,16 +92,16 @@ def check_input():
         ngpus = int(ngpus)
         if ngpus < 1:
             #print('ERROR: The "ngpus" keyword in the &control section must be a positive integer')
-            print('WARNING: The "ngpus" keyword in the &control section must be a positive integer')
+            print('WARNING: The "ngpus" keyword ({}) in the &control section must be a positive integer'.format(ngpus))
             ngpus_is_valid = False
-            #exit()
+            #exit(1)
         else:
             ngpus_is_valid = True
         ngpus_is_set = True
     else:
         ngpus_is_set = False
-    if ngpus_is_set and ngpus_is_valid:
-        print('ngpus: {}'.format(ngpus))
+    #if ngpus_is_set and ngpus_is_valid:
+    print('ngpus: {}'.format(ngpus))
 
     # gpu_type
     gpu_type = os.getenv('GPU_TYPE')
@@ -107,16 +110,16 @@ def check_input():
         valid_gpu_types = ('k20x', 'k80', 'p100', 'v100')
         if gpu_type not in valid_gpu_types:
             #print('ERROR: The "gpu_type" keyword in the &control section must be one of', valid_gpu_types)
-            print('WARNING: The "gpu_type" keyword in the &control section must be one of', valid_gpu_types)
+            print('WARNING: The "gpu_type" keyword ({}) in the &control section must be one of'.format(gpu_type), valid_gpu_types)
             gpu_type_is_valid = False
-            #exit()
+            #exit(1)
         else:
             gpu_type_is_valid = True
         gpu_type_is_set = True
     else:
         gpu_type_is_set = False
-    if gpu_type_is_set and gpu_type_is_valid:
-        print('gpu_type: {}'.format(gpu_type))
+    #if gpu_type_is_set and gpu_type_is_valid:
+    print('gpu_type: {}'.format(gpu_type))
     ########################################################################################################################
 
 
@@ -124,89 +127,89 @@ def check_input():
     # Handle deprecated "gpu_type" keyword in favor of current "worker_type" keyword
     if (not worker_type_is_set) and (not gpu_type_is_set):
         print('ERROR: The "worker_type" keyword in the &control section must be set (to one of {})'.format(valid_worker_types))
-        exit()
+        exit(1)
     if (not worker_type_is_set) and gpu_type_is_set:
-        print('WARNING: The keyword "gpu_type" is deprecated; in the future you must instead use worker_type, whose possible values are {}'.format(valid_worker_types))
+        print('WARNING: The keyword "gpu_type" is deprecated; in the future you must instead use "worker_type", whose possible values are {}'.format(valid_worker_types))
         if gpu_type_is_valid:
-            print('NOTE: We''re using the setting of gpu_type ({}), for now, as the setting for worker_type'.format(gpu_type))
+            print('NOTE: We\'re using the setting of "gpu_type" ({}), for now, as the setting for "worker_type"'.format(gpu_type))
             worker_type = gpu_type
         else:
-            print('ERROR: Attempting to use the setting of gpu_type ({}), for now, as the setting for worker_type, but it is invalid'.format(gpu_type))
-            exit()
+            print('ERROR: Attempting to use the setting of "gpu_type" ({}), for now, as the setting for "worker_type", but it is invalid'.format(gpu_type))
+            exit(1)
     if worker_type_is_set and (not gpu_type_is_set):
         if worker_type_is_valid:
             pass
         else:
             print('ERROR: The "worker_type" keyword in the &control section must be one of', valid_worker_types)
-            exit()
+            exit(1)
     if worker_type_is_set and gpu_type_is_set:
         print('ERROR: Both keywords "worker_type" and "gpu_type" are set. The former is current but the latter is deprecated; please remove "gpu_type" and re-submit.')
-        exit()
+        exit(1)
 
     # Handle deprecated "ngpus" keyword in favor of current "nworkers" keyword
     if (not nworkers_is_set) and (not ngpus_is_set):
         print('ERROR: The "nworkers" keyword in the &control section must be set (to a positive integer)')
-        exit()
+        exit(1)
     if (not nworkers_is_set) and ngpus_is_set:
-        print('WARNING: The keyword "ngpus" is deprecated; in the future you must instead use nworkers, which must be a positive integer')
+        print('WARNING: The keyword "ngpus" is deprecated; in the future you must instead use "nworkers", which must be a positive integer')
         if ngpus_is_valid:
-            print('NOTE: We''re using the setting of ngpus ({}), for now, as the setting for nworkers'.format(ngpus))
+            print('NOTE: We\'re using the setting of "ngpus" ({}), for now, as the setting for "nworkers"'.format(ngpus))
             nworkers = ngpus
         else:
-            print('ERROR: Attempting to use the setting of ngpus ({}), for now, as the setting for nworkers, but it is invalid'.format(ngpus))
-            exit()
+            print('ERROR: Attempting to use the setting of "ngpus" ({}), for now, as the setting for "nworkers", but it is invalid'.format(ngpus))
+            exit(1)
     if nworkers_is_set and (not ngpus_is_set):
         if nworkers_is_valid:
             pass
         else:
             print('ERROR: The "nworkers" keyword in the &control section must be a positive integer')
-            exit()
+            exit(1)
     if nworkers_is_set and ngpus_is_set:
         print('ERROR: Both keywords "nworkers" and "ngpus" are set. The former is current but the latter is deprecated; please remove "ngpus" and re-submit.')
-        exit()
+        exit(1)
     ########################################################################################################################
 
 
     ################ Optional keywords #####################################################################################
     # nthreads
+    nthreads = os.getenv('NTHREADS')
     if worker_type == 'cpu': # otherwise nthreads setting doesn't matter
-        nthreads = os.getenv('NTHREADS')
         if nthreads is not None:
             nthreads = int(nthreads)
             if nthreads < 1:
-                print('ERROR: The "nthreads" keyword in the &control section must be a positive integer')
-                exit()
+                print('ERROR: The "nthreads" keyword ({}) in the &control section must be a positive integer'.format(nthreads))
+                exit(1)
         else:
             print('WARNING: The keyword "nthreads" is not set in &control section; setting it to 1')
             nthreads = 1
-        print('nthreads: {}'.format(nthreads))
+    print('nthreads: {}'.format(nthreads))
 
     # custom_sbatch_args
     custom_sbatch_args = os.getenv('CUSTOM_SBATCH_ARGS', '')
     print('custom_sbatch_args: {}'.format(custom_sbatch_args))
 
     # M_gpu (GB)
-    M_gpu = float(os.getenv('MEM_PER_GPU', '30'))
+    M_gpu = int(float(os.getenv('MEM_PER_GPU', '30')))
     print('M_gpu: {}'.format(M_gpu))
 
     # M_cpu (GB)
-    M_cpu = float(os.getenv('MEM_PER_CPU', '7.5'))
+    M_cpu = int(float(os.getenv('MEM_PER_CPU', '7.5')))
     print('M_cpu: {}'.format(M_cpu))
 
     # M_swi (GB)
-    M_swi = float(os.getenv('MEM_PER_SWIFTT', '4'))
+    M_swi = int(float(os.getenv('MEM_PER_SWIFTT', '4')))
     print('M_swi: {}'.format(M_swi))
 
     # sbatch_mem_per_cpu
     sbatch_mem_per_cpu = os.getenv('SBATCH_MEM_PER_CPU')
     if sbatch_mem_per_cpu is not None:
-        sbatch_mem_per_cpu = float(sbatch_mem_per_cpu)
+        sbatch_mem_per_cpu = int(sbatch_mem_per_cpu)
     print('sbatch_mem_per_cpu: {}'.format(sbatch_mem_per_cpu))
 
     # sbatch_cpus_per_task
     sbatch_cpus_per_task = os.getenv('SBATCH_CPUS_PER_TASK')
     if sbatch_cpus_per_task is not None:
-        sbatch_cpus_per_task = int(sbatch_cpus_per_task)
+        sbatch_cpus_per_task = int(float(sbatch_cpus_per_task))
     print('sbatch_cpus_per_task: {}'.format(sbatch_cpus_per_task))
     ########################################################################################################################
 
@@ -243,7 +246,8 @@ def determine_sbatch_settings(workflow, walltime, worker_type, nworkers, nthread
             mem_per_cpu = M_cpu # practically
         else:
             mem_per_cpu = sbatch_mem_per_cpu
-        partition = 'norm'
+        #partition = 'norm'
+        partition = 'multinode'
         gres = None
         #ntasks_per_node = ntasks*[1] # per-node version
         ntasks_per_node = 1 # single-number version
@@ -273,7 +277,8 @@ def determine_sbatch_settings(workflow, walltime, worker_type, nworkers, nthread
     print('partition: {}'.format(partition))
     print('gres: {}'.format(gres))
     print('ntasks_per_node: {}'.format(ntasks_per_node))
-    print('nnodes: {}'.format(len(ntasks_per_node)))
+    #print('nnodes: {}'.format(len(ntasks_per_node))) # ideally
+    print('nnodes: {}'.format(int(round(ntasks/ntasks_per_node)))) # practically
 
     # # Output some checks; uncomment once hetergeneous sbatch jobs are implemented (see above)
     # print('')
